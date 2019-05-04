3030a63c10617667ab3ed8f817e0906099c6cb75
ebengtso <erik@jpox.org>
2019-05-04 13:53:51 +0200

    add tests

---
+++ AlgorithmUtils.Tests/TestConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AlgorithmUtils.Tests
+{
+    public class TestConfiguration
+    {
+        public static Dictionary<string, string> Parameters = new Dictionary<string, string>();
+
+        static TestConfiguration()
+        {
+            foreach (var row in File.ReadAllLines("test.settings"))
+                Parameters.Add(row.Split('=')[0], string.Join("=", row.Split('=').Skip(1).ToArray()));
+        }
+
+    }
+}
---
+++ AlgorithmUtils.Tests/UtilsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+namespace AlgorithmUtils.Tests
+{
+     public class UtilsTests
+    {
+
+        public UtilsTests()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            Utils.emailAccount = TestConfiguration.Parameters["emailAccount"];
+            Utils.emailPassword = TestConfiguration.Parameters["emailPassword"];
+            Utils.SlackURL = TestConfiguration.Parameters["SlackURL"];
+            Utils.SlackURL2 = TestConfiguration.Parameters["SlackURL2"];
+            Utils.ID = "AlgoLucy";
+        }
+        [Test]
+        public void SendSlackImage()
+        {
+        
+            var result = Utils.SendSlack(Utils.ID, "test", "Hello test!", false);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void SendSlack()
+        {
+
+            var result = Utils.SendSlackImage(Utils.ID, "test", "http://oanda.autochartist.com/aclite/imageViewer?type=CPPatternImage&uid=464650429&brokerid=232&w=1500&h=800&priceadjustment=0.0&instrumentid=0&offset=1", false);
+
+            Assert.IsTrue(result);
+
+        }
+    }
+}
